2ccfd82ab4e55c5690cc8a4cb042a3d859875116
ADAGE\ana.blanco <ana.blanco@adagetech.com>
2024-10-16 10:08:28 -0500

    Adding icon and title

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,11 +2,17 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import PostContainer from './components/PostContainer';
 import './assets/styles/style.css'
+import logo from './assets/images/bellota-logo.png';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        
+      <header className="app-header">
+          <img src={logo} alt="Logo de Bellota" className="app-logo" />
+          <h1 className="app-title">Bellota Blog</h1>
+        </header>
         <PostContainer />
       </div>
     </Provider>
