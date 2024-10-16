e08bbdb14722ecac195c498e0db2c127b212aa86
Mari2303 <mariaalejandramarinhenriquez@gmail.com>
2024-10-16 01:03:09 -0500

    Modularizando y creando estilos

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,12 +1,12 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import PostContainer from './components/PostContainer';
+import './assets/styles/style.css'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Blog Posts</h1>
         <PostContainer />
       </div>
     </Provider>
